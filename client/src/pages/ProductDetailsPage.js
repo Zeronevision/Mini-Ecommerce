5648ad045746f769a349f69cf7dd7254021b819a
Amirparsa <zeronevisionteam@gmail.com>
2025-05-19 03:42:14 +0330

    Displaying fake product details

--- client/src/pages/ProductDetailsPage.js
+++ client/src/pages/ProductDetailsPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductDetailsPage = () => {
+  const { id } = useParams();
+  //fake data
+  const product = {
+    id: id,
+    name: `Product ${id}`,
+    price: (id * 10).toFixed(2),
+    image: 'https://via.placeholder.com/300',
+    description: 'This is a great product.',
+  };
+
   return (
     <div>
-      <h1>ProductDetailsPage</h1>
+      <h1>{product.name}</h1>
+      <img src={product.image} alt={product.name} />
+      <p>Price: ${product.price}</p>
+      <p>{product.description}</p>
+      <button>Add to Cart</button>
     </div>
   );
 };
