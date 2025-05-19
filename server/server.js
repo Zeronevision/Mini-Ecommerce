4c3d04aca8dc79395fb501b7b78206300170cddd
Amirparsa <zeronevisionteam@gmail.com>
2025-05-19 16:10:31 +0330

    Configured MongoDB connection and environment variables

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
