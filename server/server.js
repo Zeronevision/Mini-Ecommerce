d0afb7fc3eeb86967f8aabc29977620a41120ae3
Amirparsa <zeronevisionteam@gmail.com>
2025-05-19 16:17:00 +0330

    Set up Express server and integrated routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
+// server/server.js
 const express = require('express');
-const connectDB = require('./config/db');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
+connectDB();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-connectDB();
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
